297f2c1c74a99ccd50356db84a6ecb86e40a990c
Ana Ramirez <anita-0621@hotmail.com>
2023-09-20 20:22:40 -0500

    implementacion chakra, formik, y yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,45 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
+import { FormControl, FormErrorMessage, FormHelperText, FormLabel, Input } from '@chakra-ui/react'
+import { useFormik } from 'formik';
+import { object, string } from 'yup';
+
+let schema = object({
+  name: string("Por favor ingrese todo en texto").min(3).required("este campo es obligatorio"),
+  email: string().email("por favor ingrese un formato correcto").required("este campo es obligatorio"),
+});
 
 function App(props) {
-  const [count, setCount] = useState(0)
-  const [tareas, setTareas] = useState([{id: 1, texto: "tarea 1"}])
-  const [tarea, setTarea] = useState("");
-  const [name, setName] = useState("Ana useState");
-
-  useEffect(()=>{
-    //alert("componente montado")
-  }, [])
-
-  useEffect(()=>{
-   // alert("componente actualizado")
-  }, [name])
-
-  const handleAddTask = () => {
-    setTareas([...tareas, {id:tareas.length + 1, texto: tarea}])
-  }
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name: '',
+    },
+    validationSchema: schema,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
   return (
     <>
-      <h1>HOME</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <FormControl isInvalid={formik.errors.name}>
+          <FormLabel>Name</FormLabel>
+          <Input type='text' name="name" onChange={formik.handleChange}
+          value={formik.values.name} />      
+          <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={formik.errors.email}>
+          <FormLabel>Email</FormLabel>
+          <Input type='email' name="email" onChange={formik.handleChange}
+          value={formik.values.email} />      
+          <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+        </FormControl>
+        
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import AppRoutes from './AppRoutes.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom"
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppRoutes />
-  </BrowserRouter>,
+  <ChakraProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </ChakraProvider>,
 )
